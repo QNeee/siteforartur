aaa57ad59700b78fb62b3e6ab6e1794585c8d087
QNeee <vovasagan7@gmail.com>
2023-05-07 12:21:51 +0300

    end

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { A, Button, ButtonContainer, Card, CardContainer, Footer, Img, P } from "./Footer.styled"
 import canabis from '../../images/canabis.jpg'
 import pistoleti from '../../images/pistols.jpg'
@@ -53,10 +53,14 @@ const data: IData[] = [
 export const FooterApp = () => {
     const [change, setChange] = useState(0);
     const [change1, setChange1] = useState(0);
+    const [hide, setHide] = useState(false);
+    const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+    const seconds = 7;
+    let intervalId: any;
     let count = 0;
-    const interval = setInterval(() => {
+    intervalId = hide ? setInterval(() => {
         count++;
-        if (count === 7) {
+        if (count === seconds) {
             if (change < data.length - 1 && change1 === 0) {
                 setChange(prev => prev + 1);
             }
@@ -69,19 +73,40 @@ export const FooterApp = () => {
             if (change > 0 && change1 > 0) {
                 setChange(prev => prev - 1);
             }
-            clearInterval(interval);
+            clearInterval(intervalId);
         }
-    }, 1000)
+    }, 1000) : clearInterval(intervalId);
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.pageYOffset;
+            const scrollingDown = currentScrollPos > prevScrollPos;
+            if (scrollingDown) {
+                setHide(true);
+                window.scrollTo({ top: document.documentElement.scrollHeight, behavior: 'auto' });
+            } else {
+                setHide(false);
+                window.scrollTo({ top: 0, behavior: 'auto' });
+            }
+            clearInterval(intervalId);
+            setPrevScrollPos(currentScrollPos);
+        };
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+
+    }, [intervalId, prevScrollPos]);
 
     const onClickNext = () => {
-        clearInterval(interval);
+        clearInterval(intervalId);
         setChange(prev => prev + 1)
     }
     const onClickPrev = () => {
-        clearInterval(interval);
+        clearInterval(intervalId);
         setChange(prev => prev - 1)
     }
-    return <Footer>
+    return <>{hide ? <Footer>
         <CardContainer>
             <Card key={data[change].id}>
                 <P>{data[change].name}</P>
@@ -89,9 +114,9 @@ export const FooterApp = () => {
                 <A href={data[change].src}>ssilka</A>
             </Card>
             <ButtonContainer>
-                {change < 5 && <Button onClick={onClickNext} >Next</Button>}
+                {change < data.length - 1 && <Button onClick={onClickNext} >Next</Button>}
                 {change !== 0 && <Button onClick={onClickPrev} >Prev</Button>}
             </ButtonContainer>
         </CardContainer>
-    </Footer>
+    </Footer> : null}</>
 }
